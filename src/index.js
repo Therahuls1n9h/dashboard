28ac6ee821baef2424269fa6827b348c3775cecd
Anıl Kan <yelkenci2004@gmail.com>
2020-12-07 19:04:18 +0300

    temp

--- src/index.js
+++ src/index.js
@@ -1,41 +1,45 @@
-import React, { Suspense } from 'react'
-import ReactDOM from 'react-dom'
-import './assets/css/tailwind.output.css'
-import App from './App'
-import { SidebarProvider } from './context/SidebarContext'
-import ThemedSuspense from './components/ThemedSuspense'
-import { Windmill } from '@windmill/react-ui'
-import * as serviceWorker from './serviceWorker'
-import { ApolloClient, InMemoryCache, ApolloProvider, HttpLink } from '@apollo/client';
-import { relayStylePagination } from '@apollo/client/utilities'
+import React, { Suspense } from "react";
+import ReactDOM from "react-dom";
+import "./assets/css/tailwind.output.css";
+import App from "./App";
+import { SidebarProvider } from "./context/SidebarContext";
+import ThemedSuspense from "./components/ThemedSuspense";
+import { Windmill } from "@windmill/react-ui";
+import * as serviceWorker from "./serviceWorker";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  HttpLink
+} from "@apollo/client";
 // if (process.env.NODE_ENV !== 'production') {
 //   const axe = require('react-axe')
 //   axe(React, ReactDOM, 1000)
 // }
 const link = new HttpLink({
-  uri: 'http://sup-boot.herokuapp.com/graphql',
+  uri: "https://sup-boot.herokuapp.com/graphql"
 });
 
 const cache = new InMemoryCache();
 
 const defaultOptions = {
   watchQuery: {
-    fetchPolicy: 'cache-and-network',
-    errorPolicy: 'all',
+    fetchPolicy: "cache-and-network",
+    errorPolicy: "all"
   },
   query: {
-    fetchPolicy: 'cache-and-network',
-    errorPolicy: 'all',
+    fetchPolicy: "cache-and-network",
+    errorPolicy: "all"
   },
   mutate: {
-    errorPolicy: 'all',
-  },
+    errorPolicy: "all"
+  }
 };
 
 const client = new ApolloClient({
   link,
   cache,
-  defaultOptions,
+  defaultOptions
 });
 
 ReactDOM.render(
@@ -48,10 +52,10 @@ ReactDOM.render(
       </Suspense>
     </SidebarProvider>
   </ApolloProvider>,
-  document.getElementById('root')
-)
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.register()
+serviceWorker.register();
